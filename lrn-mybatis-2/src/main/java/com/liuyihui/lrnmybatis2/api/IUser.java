b782a962e052bb549d9a1782f2d007537583dc89
liuyihui <makecloud@qq.com>
2016-07-21 15:17:24 +0800

    a

---
+++ lrn-mybatis-2/src/main/java/com/liuyihui/lrnmybatis2/api/IUser.java
@@ -0,0 +1,17 @@
+package com.liuyihui.lrnmybatis2.api;
+
+import com.liuyihui.lrnmybatis2.entity.User;
+
+/**
+ * 映射文件对应的接口
+ * 
+ * @author liuyh
+ */
+public interface IUser {
+	/**
+	 * 方法名为映射文件中的id
+	 * @param id
+	 * @return 
+	 */
+	public User selectUserByID(int id);
+}
